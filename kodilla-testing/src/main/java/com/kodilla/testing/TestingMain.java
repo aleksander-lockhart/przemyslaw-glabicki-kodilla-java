7819f6622d817e89ad3ea46a00d3dea71bdb931a
aleksander-lockhart <przemyslaw.glabicki@gmail.com>
2020-07-09 09:44:57 +0200

    TestingMain - Calculator.java

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,42 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+import java.sql.SQLOutput;
+import java.util.Calendar;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-    }
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Kolejny test
+        System.out.println("");
+        System.out.println("Calculator tests:");
+
+        Calculator simpleCalculator = new Calculator(10, 5);
+         int calcAddResult = 15;
+         int calcSubstractResult = 5;
+
+         if (calcAddResult == simpleCalculator.addAtoB()) {
+             System.out.println("Add OK");
+         } else {
+             System.out.println("Add error");
+         }
+
+         if (calcSubstractResult == simpleCalculator.substractAfromB()) {
+             System.out.println("Substract OK");
+         } else {
+             System.out.println("Substract error");
+                 }
+             }
+
 }
