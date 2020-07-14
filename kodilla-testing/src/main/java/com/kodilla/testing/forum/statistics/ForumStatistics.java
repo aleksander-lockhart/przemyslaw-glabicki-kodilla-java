dc2f0ab674a819ce72ba66dcdfac55b24fb784fb
aleksander-lockhart <przemyslaw.glabicki@gmail.com>
2020-07-14 09:11:51 +0200

    6.5. Mockito, mockujemy obiekty

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,62 @@
+package com.kodilla.testing.forum.statistics;
+
+public class ForumStatistics {
+    private Statistics statistics;
+    private int userQnt;
+    private int postsQnt;
+    private int commentsQnt;
+    private double postPerUser;
+    private double commentPerUser;
+    private double commentPerPost;
+
+    public ForumStatistics(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public void calculateAdvStatistics() {
+        userQnt = statistics.usersNames().size();
+        postsQnt = statistics.postsCount();
+        commentsQnt = statistics.commentsCount();
+        postPerUser = ((double)statistics.postsCount()) / statistics.usersNames().size();
+
+        if (commentPerUser == 0) {
+            commentPerUser = 0;
+        } else {
+            commentPerUser = ((double)statistics.usersNames().size()) / userQnt;
+            }
+
+        if (postsQnt == 0) {
+            commentPerPost = 0;
+        } else {
+            commentPerPost = ((double)statistics.commentsCount()) / postsQnt;
+        }
+    }
+
+    public Statistics getStatistics() {
+        return statistics;
+    }
+
+    public int getUserQnt() {
+        return userQnt;
+    }
+
+    public int getPostsQnt() {
+        return postsQnt;
+    }
+
+    public int getCommentsQnt() {
+        return commentsQnt;
+    }
+
+    public double getPostPerUser() {
+        return postPerUser;
+    }
+
+    public double getCommentPerUser() {
+        return commentPerUser;
+    }
+
+    public double getCommentPerPost() {
+        return commentPerPost;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculateAdvStatisticsTestSuite.java
@@ -0,0 +1,124 @@
+package com.kodilla.testing.forum.statistics;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CalculateAdvStatisticsTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("Test start");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("End of tests");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Test #" + testCounter);
+    }
+
+    @Test
+    public void testIfPostCountZero() {
+        //Given
+        Statistics statisticMock = mock(Statistics.class);
+        when(statisticMock.postsCount()).thenReturn(0);
+        ForumStatistics compute = new ForumStatistics(statisticMock);
+        //When
+        compute.calculateAdvStatistics();
+        //Then
+        Assert.assertEquals(0, compute.getPostsQnt());
+    }
+
+    @Test
+    public void testIfPostCount1000() {
+        //Given
+        Statistics statisticMock = mock(Statistics.class);
+        when(statisticMock.postsCount()).thenReturn(1000);
+        ForumStatistics compute = new ForumStatistics(statisticMock);
+        //When
+        compute.calculateAdvStatistics();
+        //Then
+        Assert.assertEquals(1000, compute.getPostsQnt());
+    }
+
+    @Test
+    public void testIfCommentsCountZero() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+        ForumStatistics compute = new ForumStatistics(statisticsMock);
+        //When
+        compute.calculateAdvStatistics();
+        //Then
+        Assert.assertEquals(0, compute.getCommentsQnt());
+    }
+
+    @Test
+    public void testIfMoreCommentsThenPost() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        ForumStatistics compute = new ForumStatistics(statisticsMock);
+        when(statisticsMock.postsCount()).thenReturn(100);
+        when(statisticsMock.commentsCount()).thenReturn(1000);
+        //When
+        compute.calculateAdvStatistics();
+        //Then
+        Assert.assertEquals(10, compute.getCommentPerPost(), 0);
+    }
+
+    @Test
+    public void testIfMorePostsThenComments() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        ForumStatistics compute = new ForumStatistics(statisticsMock);
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(500);
+        //When
+        compute.calculateAdvStatistics();
+        //Then
+        Assert.assertEquals(0.5, compute.getCommentPerPost(), 0);
+    }
+
+    @Test
+    public void testUserCountZero() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        ForumStatistics compute = new ForumStatistics(statisticsMock);
+        ArrayList<String> users = new ArrayList<>();
+        when(statisticsMock.usersNames()).thenReturn(users);
+
+        //When
+        compute.calculateAdvStatistics();
+        //Then
+        Assert.assertEquals(0, compute.getUserQnt(), 0);
+    }
+
+    @Test
+    public void testUserCount100() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        ForumStatistics compute = new ForumStatistics(statisticsMock);
+        ArrayList<String> users = new ArrayList<>();
+        for (int i = 0; i <100; i++) {
+            users.add("User" + i);
+        }
+        when(statisticsMock.usersNames()).thenReturn(users);
+
+        //When
+        compute.calculateAdvStatistics();
+        //Then
+        Assert.assertEquals(100, compute.getUserQnt(), 0);
+    }
+
+
+
+
+}
