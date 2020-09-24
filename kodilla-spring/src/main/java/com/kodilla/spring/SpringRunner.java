692681c30cccdda0ebaee3525aeb67aeefd228b9
aleksander-lockhart <przemyslaw.glabicki@gmail.com>
2020-09-24 13:14:03 +0200

    Spring start

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+    @SpringBootApplication
+    public class SpringRunner {
+
+        public static void mail(String args[]) {
+            SpringApplication.run(com.kodilla.spring.SpringRunner.class, args);
+        }
+    }
+
